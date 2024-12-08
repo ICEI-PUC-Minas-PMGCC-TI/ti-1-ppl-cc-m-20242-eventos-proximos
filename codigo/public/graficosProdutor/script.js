c97bd783e4643df12f6ed7494dd97a61c4b6932b
Rafael Henrique Silva <rhenriques.2005@gmail.com>
2024-12-08 14:33:39 -0300

    calendario e cabeçalho

--- codigo/public/graficosProdutor/script.js
+++ codigo/public/graficosProdutor/script.js
@@ -211,3 +211,21 @@ document.addEventListener('DOMContentLoaded', () => {
     createFavoritedEventsChart('graficoFavoritos');
     createConfirmedEventsChart('graficoConfirmacoes');
 });
+
+async function fetchUserName() {
+    try {
+        const response = await fetch("http://localhost:3000/usuarios"); 
+        const data = await response.json();
+  
+        if (data.length > 0) { 
+            const userName = data[0].nome; 
+            document.getElementById('user-name').textContent = `Olá, ${userName}`;
+        } else {
+            console.warn('Nenhum usuário encontrado');
+        }
+    } catch (error) {
+        console.error('Erro ao buscar o nome do usuário:', error);
+    }
+}
+
+fetchUserName();
