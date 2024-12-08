58352848ac9a20ef57133100302f6cf016d0e788
Caio Fernandes da Silva <caio.silva.1543482@sga.pucminas.br>
2024-12-08 16:31:21 -0300

    linkando calendario com index e tela do usuario

--- codigo/public/calendario/script.js
+++ codigo/public/calendario/script.js
@@ -216,4 +216,22 @@ function initCalendar() {
     monthYearSelect.addEventListener('change', selectMonthYear);
 }
 
+async function fetchUserName() {
+    try {
+        const response = await fetch("http://localhost:3000/usuarios"); 
+        const data = await response.json();
+  
+        if (data.length > 0) { 
+            const userName = data[0].nome; 
+            document.getElementById('user-name').textContent = `Olá, ${userName}`;
+        } else {
+            console.warn('Nenhum usuário encontrado');
+        }
+    } catch (error) {
+        console.error('Erro ao buscar o nome do usuário:', error);
+    }
+}
+
+fetchUserName();
+
 document.addEventListener('DOMContentLoaded', fetchData);
