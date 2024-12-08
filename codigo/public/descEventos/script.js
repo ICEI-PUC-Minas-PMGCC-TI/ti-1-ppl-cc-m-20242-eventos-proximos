142c64bd03779d27b5e7625947b848040aa64a0a
Jopefv <joaopedrofviana22@gmail.com>
2024-12-08 19:32:51 -0300

    calendario update

--- codigo/public/descEventos/script.js
+++ codigo/public/descEventos/script.js
@@ -134,3 +134,21 @@ function setNeutral() {
 
 // Chama a função para buscar os detalhes do evento
 fetchEventDetails();
+
+async function fetchUserName() {
+  try {
+      const response = await fetch("http://localhost:3000/usuarios"); 
+      const data = await response.json();
+
+      if (data.length > 0) { 
+          const userName = data[0].nome; 
+          document.getElementById('user-name').textContent = `Olá, ${userName}`;
+      } else {
+          console.warn('Nenhum usuário encontrado');
+      }
+  } catch (error) {
+      console.error('Erro ao buscar o nome do usuário:', error);
+  }
+}
+
+fetchUserName();
